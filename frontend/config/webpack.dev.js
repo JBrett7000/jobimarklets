e3258450adffc9e5b3a1536e8877da8640326232
Ndifreke Ekott <ndy40.ekott@gmail.com>
2017-03-09 07:41:29 +0000

    [TC-1] Added watch command

--- frontend/config/webpack.dev.js
+++ frontend/config/webpack.dev.js
@@ -5,7 +5,6 @@ var helpers = require('./helpers');
 
 module.exports = webpackMerge(commonConfig, {
   devtool: 'cheap-module-eval-source-map',
-
   output: {
     path: '../backend-service/public/assets',
     filename: '[name].js',
